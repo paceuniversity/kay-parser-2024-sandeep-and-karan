a90919349cc1814712537a2ff163a68bae9b487f
hideonfruit <148816660+hideonfruit@users.noreply.github.com>
2024-12-15 03:58:06 -0500

    Update Program.java

--- src/main/java/com/scanner/project/Program.java
+++ src/main/java/com/scanner/project/Program.java
@@ -1,9 +1,8 @@
-package com.scanner.project;
 // Program.java
 
 // NOT TO BE MODIFIED (except if you are sure there is a bug!)
 
-// Abstract syntax of JAY but also for KAY
+// Abstract syntax of JAY 
 
 // Display methods are added to facilitate debugging and experimentation
 
@@ -75,11 +74,11 @@ class Declaration {
 }
 
 class Type {
-	// Type = integer | bool | undef
+	// Type = int | bool | undef
 
 	public String id;
 
-	final static String INTEGER = "integer";
+	final static String INTEGER = "int";
 	final static String BOOLEAN = "bool";
 	final static String UNDEFINED = "undef";
 
@@ -271,7 +270,7 @@ class Unary extends Expression {
 
 class Operator {
 
-	public String val; // value of the operator - one of the constant 
+	public String val; // value of the operator - one of the constant above
 
 	// Operator = BooleanOp | RelationalOp | ArithmeticOp | Unary1Op
 	// BooleanOp = && | ||
